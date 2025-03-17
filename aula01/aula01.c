c8e6d6e71e8d8d55875112e7c0f7b1215216dc67
Phelipe <phelipe.silva@sptech.school>
2025-03-16 21:52:52 -0300

    aula 01 do curso - comandos iniciais

---
+++ aula01/aula01.c
@@ -0,0 +1,19 @@
+// --- Bibliotecas Iniciais ---
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    --- CURSO LINGUAGEM C ANSI ---
+    Professor: Engenheiro Wagner Rambo
+    Autor: Phelipe Bruione da Silva
+    Objetivo do programa: Primeiro aula de linguagem C, com comandos iniciais e mensagem Hello World
+    Dia do programa: 16/03/2025
+*/
+// --- Função Principal ---
+int main()
+{
+    // --- Declaração das variáveis ---
+    
+    printf("HELLO, WORLD!\n");
+
+    return 0;
+} // end main
